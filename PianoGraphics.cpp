b3d04587a3948fa4a74361f95c78fc9abcd5f426
kefah123 <kefah@uvm.edu>
2020-04-30 15:16:10 -0400

    Initialized graphics file for project

---
+++ PianoGraphics.cpp
@@ -0,0 +1,112 @@
+
+
+#include "PianoGraphics.h"
+#include <iostream>
+#include <vector>
+using namespace std;
+GLdouble width, height;
+int wd;
+void init() {
+    width = 750;
+    height = 500;
+}
+
+/* Initialize OpenGL Graphics */
+void initGL() {
+    // Set "clearing" or background color
+    glClearColor(0.0f, 0.0f, 0.0f, 1.0f); // Black and opaque
+}
+
+/* Handler for window-repaint event. Call back when the window first appears and
+ whenever the window needs to be re-painted. */
+void display() {
+    // tell OpenGL to use the whole window for drawing
+    glViewport(0, 0, width, height);
+    glMatrixMode(GL_PROJECTION);
+    glLoadIdentity();
+    glOrtho(0.0, width, height, 0.0, -1.f, 1.f);
+    glClear(GL_COLOR_BUFFER_BIT);
+    glPolygonMode(GL_FRONT_AND_BACK, GL_FILL);
+    glFlush();
+
+}
+
+void kbd(unsigned char key, int x, int y)
+{
+    // escape
+    if (key == 27) {
+        glutDestroyWindow(wd);
+        exit(0);
+    }
+
+    glutPostRedisplay();
+}
+
+void kbdS(int key, int x, int y) {
+    switch(key) {
+        case GLUT_KEY_DOWN:
+            break;
+        case GLUT_KEY_LEFT:
+            break;
+        case GLUT_KEY_RIGHT:
+            break;
+        case GLUT_KEY_UP:
+            break;
+    }
+    glutPostRedisplay();
+}
+
+
+void cursor(int x, int y) {
+    glutPostRedisplay();
+}
+
+void mouse(int button, int state, int x, int y) {
+    glutPostRedisplay();
+}
+
+void timer(int dummy) {
+    glutPostRedisplay();
+    glutTimerFunc(30, timer, dummy);
+}
+
+/* Main function: GLUT runs as a console application starting at main()  */
+int main(int argc, char** argv) {
+
+    init();
+
+    glutInit(&argc, argv);          // Initialize GLUT
+
+    glutInitDisplayMode(GLUT_RGBA);
+
+    glutInitWindowSize((int)width, (int)height);
+    glutInitWindowPosition(100, 200); // Position the window's initial top-left corner
+    /* create the window and store the handle to it */
+    wd = glutCreateWindow("Virtual Piano!");
+
+    // Register callback handler for window re-paint event
+    glutDisplayFunc(display);
+
+    // Our own OpenGL initialization
+    initGL();
+
+    // register keyboard press event processing function
+    // works for numbers, letters, spacebar, etc.
+    glutKeyboardFunc(kbd);
+
+    // register special event: function keys, arrows, etc.
+    glutSpecialFunc(kbdS);
+
+    // handles mouse movement
+    glutPassiveMotionFunc(cursor);
+
+    // handles mouse click
+    glutMouseFunc(mouse);
+
+    // handles timer
+    glutTimerFunc(0, timer, 0);
+
+    // Enter the event-processing loop
+    glutMainLoop();
+    return 0;
+}
